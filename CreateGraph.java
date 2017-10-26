e776ba1b1e82c0137a84055d5817549a8a7d7346
Akshay Sharma <akshay10791@gmail.com>
2017-10-26 17:13:51 -0400

    Added sequential program

---
+++ CreateGraph.java
@@ -0,0 +1,48 @@
+import edu.rit.util.*;
+import java.util.Set;
+import java.util.*;
+import java.io.*;
+
+public class CreateGraph {
+    String path;
+    int v,e;
+
+    CreateGraph(String s) {
+        this.path = s;
+    }
+
+    public HashSet[] GenerateGraph() throws Exception {
+        Scanner sc;
+
+            sc = new Scanner(new File(path));
+
+        this.v = (sc.nextInt());
+       // List<Set<Integer>>[] g = new List[f];
+        HashSet<Integer>[] g = new HashSet[v];
+
+        for(int i=0;i<v;i++)
+            g[i]= new HashSet();
+
+        this.e = sc.nextInt();
+        for (int i = 0; i < e; i++) {
+            int a, b;
+            a = sc.nextInt();
+            b = sc.nextInt();
+            g[a-1].add(b-1);
+            g[b-1].add(a-1);
+
+        }
+
+        return g;
+
+    }
+
+    public HashSet<Integer> getHashSet(){
+        HashSet<Integer> temp = new HashSet<>();
+        for(int i=0;i<v;i++)
+            temp.add(i);
+
+    return temp;
+    }
+
+}
---
+++ MaximumClique.java
@@ -0,0 +1,65 @@
+import edu.rit.util.IntList;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class MaximumClique {
+    static HashSet[] graph;
+    static int size = 0;
+    static HashSet<Integer> maximum;
+
+    public static void main(String[] s) {
+
+
+        //CreateGraph g = new CreateGraph("D:\\RIT\\SEM3\\Parallel Prog\\project\\4N4E.txt");
+        CreateGraph g = new CreateGraph("D:\\RIT\\SEM3\\Parallel Prog\\project\\200N987E.txt");
+
+        try {
+            graph = g.GenerateGraph();
+            HashSet<Integer> P = g.getHashSet();
+            BronKerbosch1(new HashSet<Integer>(), P, new HashSet<Integer>());
+
+            for (Integer I : maximum) {
+                System.out.print(I + " ");
+            }
+
+            System.out.println("\\n"+size);
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+
+    public static void BronKerbosch1(HashSet<Integer> R, HashSet<Integer> P, HashSet<Integer> X) {
+
+        if (P.isEmpty() && X.isEmpty()) {
+            if (size < R.size()) {
+                maximum = (HashSet) R.clone();
+                size = R.size();
+            }
+
+        }
+
+        while (!P.isEmpty()) {
+            Integer i;
+            {
+                Iterator<Integer> itr = P.iterator();
+                i = itr.next();
+            }
+
+            HashSet<Integer> R2 = (HashSet) R.clone();
+            HashSet<Integer> P2 = (HashSet) P.clone();
+            HashSet<Integer> X2 = (HashSet) X.clone();
+            P2.retainAll(graph[i]);
+            X2.retainAll(graph[i]);
+            R2.add(i);
+            BronKerbosch1(R2, P2, X2);
+            P.remove(i);
+            X.add(i);
+
+        }
+    }
+}
+
