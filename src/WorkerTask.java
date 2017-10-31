43093cb34a82715cadf027e95d81a14b1f5b44eb
Vishal Kole <vishalvkole@gmail.com>
2017-10-31 13:27:49 -0400

    created Worker file to compute and reduce the BK_Algo. need to add the terminating condition

--- src/WorkerTask.java
+++ src/WorkerTask.java
@@ -1,5 +1,5 @@
+import edu.rit.pj2.Loop;
 import edu.rit.pj2.Task;
-import edu.rit.pj2.Tuple;
 import edu.rit.pj2.TupleListener;
 import edu.rit.pj2.tuple.EmptyTuple;
 import edu.rit.pj2.tuple.ObjectArrayTuple;
@@ -11,17 +11,35 @@ public class WorkerTask extends Task {
     HashSet[] graph;
     boolean isMainJobDone;
     boolean areAllBKTuplesProcessed;
+    MaximumCliqueVBL reductionVBL;
 
     @Override
     public void main(String[] strings) throws Exception {
+
+        reductionVBL = new MaximumCliqueVBL();
         graph = readTuple(new ObjectArrayTuple<HashSet>()).item;
 
-        addTupleListener(new TupleListener<EmptyTuple>(new EmptyTuple())
-        {
-            public void run(EmptyTuple tuple)
-            {
+        addTupleListener(new TupleListener<EmptyTuple>(new EmptyTuple()) {
+            public void run(EmptyTuple tuple) {
                 isMainJobDone = true;
             }
         });
+
+
+        workerFor().schedule(guided).exec(new Loop() {
+
+            MaximumCliqueVBL thrreductionVBL;
+            BronKerbosch algo = new BronKerbosch(graph);
+            BKConfig state;
+
+            @Override
+            public void run(int i) throws Exception {
+                state = readTuple((new BKConfig()));
+                algo.runBronKerbosch(state.R, state.P, state.X);
+                thrreductionVBL.reduce(algo.size, algo.maximum);
+            }
+        });
+
+        putTuple(reductionVBL);
     }
 }
