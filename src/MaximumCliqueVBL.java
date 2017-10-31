755a2ba886bf10b0de2afab26519f4b408d4d57b
Vishal Kole <vishalvkole@gmail.com>
2017-10-30 21:11:16 -0400

    Completed the logic for MaximumCliqueVBL

--- src/MaximumCliqueVBL.java
+++ src/MaximumCliqueVBL.java
@@ -16,22 +16,55 @@ public class MaximumCliqueVBL extends Tuple implements Vbl {
     }
 
     @Override
-    public void writeOut(OutStream outStream) throws IOException {
+    public Object clone() {
+        try {
+            MaximumCliqueVBL vbl = (MaximumCliqueVBL) super.clone();
+            vbl.size = this.size;
+            vbl.maximum = (HashSet) this.maximum.clone();
+            return vbl;
+        } catch (Exception ex) {
+            throw new RuntimeException("GraphRadiusVBL clone error");
+        }
+    }
 
+
+    @Override
+    public void writeOut(OutStream outStream) throws IOException {
+        outStream.writeLong(size);
+        for (Integer i : maximum)
+            outStream.writeLong(i);
     }
 
     @Override
     public void readIn(InStream inStream) throws IOException {
-
+        this.size = inStream.readInt();
+        maximum.clear();
+        for (int i = 0; i < size; i++)
+            maximum.add(inStream.readInt());
     }
 
     @Override
     public void set(Vbl vbl) {
-
+        MaximumCliqueVBL VBLvar = (MaximumCliqueVBL) vbl;
+        this.size = VBLvar.size;
+        this.maximum = VBLvar.maximum;
     }
 
     @Override
     public void reduce(Vbl vbl) {
+        MaximumCliqueVBL convVBL = (MaximumCliqueVBL) vbl;
+        if (this.size < convVBL.size) {
+            this.maximum.clear();
+            for (Integer i : convVBL.maximum)
+                maximum.add(i);
+        }
+    }
 
+    public void reduce(int size, HashSet<Integer> hset) {
+        if (this.size < size) {
+            this.maximum.clear();
+            for (Integer i : hset)
+`maximum.add(i);
+        }
     }
 }
