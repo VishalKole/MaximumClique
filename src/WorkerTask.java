daf72873514c1d2e9c3c2ccd8cc8c8589c996627
Akshay Sharma <akshay10791@gmail.com>
2017-10-30 20:34:28 -0400

    Main job publishes the graph as a tuple and worker tasks read this tuple + main job publishes an empty tuple and workers listen for this event to set a field

--- src/WorkerTask.java
+++ src/WorkerTask.java
@@ -1,8 +1,27 @@
 import edu.rit.pj2.Task;
+import edu.rit.pj2.Tuple;
+import edu.rit.pj2.TupleListener;
+import edu.rit.pj2.tuple.EmptyTuple;
+import edu.rit.pj2.tuple.ObjectArrayTuple;
+
+import java.util.HashSet;
 
 public class WorkerTask extends Task {
+
+    HashSet[] graph;
+    boolean isMainJobDone;
+    boolean areAllBKTuplesProcessed;
+
     @Override
     public void main(String[] strings) throws Exception {
+        graph = readTuple(new ObjectArrayTuple<HashSet>()).item;
 
+        addTupleListener(new TupleListener<EmptyTuple>(new EmptyTuple())
+        {
+            public void run(EmptyTuple tuple)
+            {
+                isMainJobDone = true;
+            }
+        });
     }
 }
