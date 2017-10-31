cc0bab998855249267c61f74ef9bc09c972eae9c
Vishal Kole <vishalvkole@gmail.com>
2017-10-30 21:30:37 -0400

    ReduceTask.java completed

--- src/ReduceTask.java
+++ src/ReduceTask.java
@@ -3,6 +3,20 @@ import edu.rit.pj2.Task;
 public class ReduceTask extends Task {
     @Override
     public void main(String[] strings) throws Exception {
+        MaximumCliqueVBL VBL = new MaximumCliqueVBL();
 
+        MaximumCliqueVBL templateVBL = new MaximumCliqueVBL();
+
+        MaximumCliqueVBL StorageVBL = new MaximumCliqueVBL();
+
+        while ((StorageVBL = tryToTakeTuple(templateVBL)) != null)
+            VBL.reduce(StorageVBL);
+
+        System.out.println("Maximum Clique Size: " + VBL.size);
+        System.out.println("\n");
+        System.out.println("Vertices in the maximum clique: \n");
+        for (Integer I : VBL.maximum) {
+            System.out.print(I + " ");
+        }
     }
 }
