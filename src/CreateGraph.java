a61534764a4ac372ed857e6fd86cf1265a9106ac
Srinath Obla <sso8260@rit.edu>
2017-11-03 18:22:54 -0400

    Code reformatting

--- src/CreateGraph.java
+++ src/CreateGraph.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class CreateGraph {
     String path;
-    int v,e;
+    int v, e;
 
     CreateGraph(String s) {
         this.path = s;
@@ -17,26 +17,26 @@ public class CreateGraph {
         this.v = (sc.nextInt());
         HashSet<Integer>[] g = new HashSet[v];
 
-        for(int i=0;i<v;i++)
-            g[i]= new HashSet();
+        for (int i = 0; i < v; i++)
+            g[i] = new HashSet();
 
         this.e = sc.nextInt();
         for (int i = 0; i < e; i++) {
             int a, b;
             a = sc.nextInt();
             b = sc.nextInt();
-            g[a-1].add(b-1);
-            g[b-1].add(a-1);
+            g[a - 1].add(b - 1);
+            g[b - 1].add(a - 1);
         }
         return g;
     }
 
-    public HashSet<Integer> getHashSet(){
+    public HashSet<Integer> getHashSet() {
         HashSet<Integer> temp = new HashSet<>();
-        for(int i=0;i<v;i++)
+        for (int i = 0; i < v; i++)
             temp.add(i);
 
-    return temp;
+        return temp;
     }
 
 }
