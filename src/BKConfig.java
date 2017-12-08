7d9f85cc0c8009eaff0be048a4ea12d78e10adb5
Akshay Sharma <akshay10791@gmail.com>
2017-12-08 15:03:41 -0500

    Added documentation for BKConfig

--- src/BKConfig.java
+++ src/BKConfig.java
@@ -14,15 +14,33 @@ import edu.rit.pj2.Tuple;
 import java.io.IOException;
 import java.util.HashSet;
 
+/**
+ * This class holds information regarding the configuration for each step
+ * of the Bron-Kerbosch algorithm and functionality associated with it.
+ * <P>
+ *
+ * @author
+ * @version
+ */
 public class BKConfig extends Tuple {
 
     HashSet<Integer> R;
     HashSet<Integer> P;
     HashSet<Integer> X;
 
+    /**
+     * This is the default constructor for the class.
+     */
     BKConfig() {
     }
 
+    /**
+     * This is the paramterized constructor for the class.
+     *
+     * @param r  Set R.
+     * @param p  Set P.
+     * @param x  Set X.
+     */
     BKConfig(HashSet<Integer> r, HashSet<Integer> p, HashSet<Integer> x)
     {
         this.R = r;
@@ -30,6 +48,12 @@ public class BKConfig extends Tuple {
         this.X = x;
     }
 
+    /**
+     * Writes the values of the object to the given out stream.
+     *
+     * @param outStream     The out stream
+     * @throws IOException  Thrown if an I/O error occurs.
+     */
     @Override
     public void writeOut(OutStream outStream) throws IOException {
         outStream.writeObject(R);
@@ -37,6 +61,12 @@ public class BKConfig extends Tuple {
         outStream.writeObject(X);
     }
 
+    /**
+     * Reads the values of object from the in stream.
+     *
+     * @param inStream      The in stream.
+     * @throws IOException  Thrown if an IO error occurs.
+     */
     @Override
     public void readIn(InStream inStream) throws IOException {
         this.R = (HashSet<Integer>) inStream.readObject();
@@ -44,14 +74,29 @@ public class BKConfig extends Tuple {
         this.X = (HashSet<Integer>) inStream.readObject();
     }
 
+    /**
+     * This function retrieves set P.
+     *
+     * @return  Set P.
+     */
     public HashSet<Integer> getP() {
         return P;
     }
 
+    /**
+     * This function retrieves set X.
+     *
+     * @return  Set X.
+     */
     public HashSet<Integer> getX() {
         return X;
     }
 
+    /**
+     * This function retrieves set R.
+     *
+     * @return  Set R.
+     */
     public HashSet<Integer> getR() {
         return R;
     }
