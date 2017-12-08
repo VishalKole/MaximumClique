7df8671b0adf0c2ac23887bcaf67c3b6f340eb82
Akshay Sharma <akshay10791@gmail.com>
2017-12-08 15:38:19 -0500

    Commented MulticoreParallel (Both)

--- src/MaximumCliqueMulticoreParallel.java
+++ src/MaximumCliqueMulticoreParallel.java
@@ -12,35 +12,60 @@ import edu.rit.pj2.Task;
 import java.util.HashSet;
 
 
-
+/**
+ * This class implements version 1 of the multicore parallel version of the algorithm.
+ * It contains all the data and functionality required.
+ */
 public class MaximumCliqueMulticoreParallel extends Task {
-
 
     private HashSet<Integer>[] graph;
 
+    /**
+     * This is the main function to be run.
+     *
+     * @param strings     Contains the arguments.
+     * @throws Exception  Throws all Exception that might occur.
+     */
     @Override
     public void main(String[] strings) throws Exception {
+
+        //Argument check.
         if(strings.length < 1){
             System.out.println("Not enough arguments");
             usage();
             terminate(1);
         }
+
+        //Create the graph.
         CreateGraph g = new CreateGraph(strings[0]);
         graph = g.GenerateGraph();
 
+        //Set up reducer.
         MaximumCliqueVBL masterReducer = new MaximumCliqueVBL();
 
+        //Distribute among the cores.
         parallelFor(0, graph.length - 1).schedule(guided).chunk(1).exec (new Loop() {
 
             MaximumCliqueVBL thrReducer;
             BronKerbosch algo;
 
+            /**
+             * This function sets up the thread local fields.
+             *
+             * @throws Exception Throws all Exceptions.
+             */
             @Override
             public void start() throws Exception {
                 thrReducer = threadLocal(masterReducer);
                 algo = new BronKerbosch(graph);
             }
 
+            /**
+             * This function runs the algorithm.
+             *
+             * @param i            The vertex.
+             * @throws Exception   Throws all Exceptions.
+             */
             @Override
             public void run(int i) throws Exception {
 
@@ -49,17 +74,32 @@ public class MaximumCliqueMulticoreParallel extends Task {
             }
         });
 
+        //Debug outputs.
         System.out.println("Maximum Clique Size: " + masterReducer.size);
         System.out.println("\n");
         System.out.println("Vertices in the maximum clique: \n");
+
+        //Can view progress through the graph vertices.
         for (Integer I : masterReducer.maximum) {
             System.out.print(I + " ");
         }
-
     }
 
+    /**
+     * This function displays an error when there is a problem with the arguments passed in.
+     */
     private static void usage(){
         System.out.println("Usage on non cluster: java pj2 MaximumCliqueMulticoreParallel <source_file_path> \n" +
                 "Usage on cluster computer: java pj2 jar=jarfile.jar MaximumCliqueMulticoreParallel <source_file_path>");
+    }
+
+    /**
+     * Number of cores required.
+     *
+     * @return Number of cores.
+     */
+    protected static int coresRequired()
+    {
+        return 1;
     }
 }
--- src/MaximumCliqueMulticoreParallel2.java
+++ src/MaximumCliqueMulticoreParallel2.java
@@ -11,35 +11,61 @@ import edu.rit.pj2.Loop;
 import edu.rit.pj2.Task;
 import java.util.HashSet;
 
-
+/**
+ * This class implements version 2 of the multicore parallel version of the algorithm.
+ * It contains all the data and functionality required.
+ */
 public class MaximumCliqueMulticoreParallel2 extends Task {
 
 
     private HashSet<Integer>[] graph;
 
+    /**
+     * This is the main function to be run.
+     *
+     * @param strings     Contains the arguments.
+     * @throws Exception  Throws all Exception that might occur.
+     */
     @Override
     public void main(String[] strings) throws Exception {
+
+        //Argument check.
         if(strings.length < 1){
             System.out.println("Not enough arguments");
             usage();
             terminate(1);
         }
+
+        //Graph creation.
         CreateGraph g = new CreateGraph(strings[0]);
         graph = g.GenerateGraph();
 
+        //Set up reducer.
         MaximumCliqueVBL masterReducer = new MaximumCliqueVBL();
 
+        //Distribute the vertices.
         parallelFor(0, (graph.length*(graph.length-1)) - 1).schedule(guided).chunk(1).exec (new Loop() {
 
             MaximumCliqueVBL thrReducer;
             BronKerbosch algo;
 
+            /**
+             * This function sets up the thread local fields.
+             *
+             * @throws Exception Throws all Exceptions.
+             */
             @Override
             public void start() throws Exception {
                 thrReducer = threadLocal(masterReducer);
                 algo = new BronKerbosch(graph);
             }
 
+            /**
+             * This function runs the algorithm.
+             *
+             * @param i            The vertex.
+             * @throws Exception   Throws all Exceptions.
+             */
             @Override
             public void run(int i) throws Exception {
 
@@ -48,20 +74,30 @@ public class MaximumCliqueMulticoreParallel2 extends Task {
             }
         });
 
+        //Debug outputs.
         System.out.println("Maximum Clique Size: " + masterReducer.size);
         System.out.println("\n");
         System.out.println("Vertices in the maximum clique: \n");
+
+        //Can view progress through the graph vertices.
         for (Integer I : masterReducer.maximum) {
             System.out.print(I + " ");
         }
-
     }
 
+    /**
+     * This function displays an error when there is a problem with the arguments passed in.
+     */
     private static void usage(){
         System.out.println("Usage on non cluster: java pj2 MaximumCliqueMulticoreParallel <source_file_path> \n" +
                 "Usage on cluster computer: java pj2 jar=jarfile.jar MaximumCliqueMulticoreParallel <source_file_path>");
     }
 
+    /**
+     * Number of cores required.
+     *
+     * @return Number of cores.
+     */
     protected static int coresRequired()
     {
         return 1;
